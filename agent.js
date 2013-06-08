3143da8c3a2baef238c64f4dc113cce93a3e39ab
Tony Boyles <AABoyles@gmail.com>
2013-06-08 16:16:46 -0400

    Giving a little more shape to Agent.js

--- agent.js
+++ agent.js
@@ -1,39 +1,61 @@
 var stepTime = 1;
 var col = undefined;
 
-function stepGame() {
-  col.each(function(e) {
-    e.computeNextState();
-  });
-  col.each(function(e) {
-    e.transition();
-  });
-  setTimeout("stepGame()", stepTime);
+function step() {
+	col.each(function(e) {
+		e.computeNextState();
+	});
+	col.each(function(e) {
+		e.transition();
+	});
+	setTimeout("step()", stepTime);
 }
 
-$(function() {
-  var models = new Array();
-  var trs = $(document.createElement('tbody'));
-  for (var x = 0; x < 100; x++) {
-    var tr = $(document.createElement('tr'));
-    for (var y = 0; y < 100; y++) {
-      var alive = (Math.floor(Math.random() * 100) % 2) == 1;
-      var cell = new Cell({
-        'x' : x,
-        'y' : y,
-        'alive' : alive
-      });
-      models.push(cell);
-      var cv = new CellView({
-        model : cell
-      });
-      cv.render();
-      tr.append(cv.el);
-    }
-    trs.append(tr)
-  }
-  $('#world').html(trs)
-  col = new CellCollection(models);
+var Agent = Backbone.Model;
 
-  setTimeout("stepGame()", stepTime);
+var Population = Backbone.Collection;
+
+var ABM = function(domElement, collections) {
+	run = function() {};
+	geometry = {
+		bounded : {},
+		verticalWrap : {},
+		horizontalWrap : {},
+		taurus : {},
+		sphere : {}
+	};
+    var $el = domElement || $("body");
+    var colls = collections || [];
+};
+
+//myAgent = new Agent('div', {attributes}, {options});
+//myAgents = new Population(myAgent, {options});
+//myModel = new ABM($('someDOMElement'), [myAgents]);
+//myModel.run();
+
+$(function() {
+	var models = new Array();
+	var trs = $(document.createElement('tbody'));
+	for (var x = 0; x < 100; x++) {
+		var tr = $(document.createElement('tr'));
+		for (var y = 0; y < 100; y++) {
+			var alive = (Math.floor(Math.random() * 100) % 2) == 1;
+			var cell = new Cell({
+				'x' : x,
+				'y' : y,
+				'alive' : alive
+			});
+			models.push(cell);
+			var cv = new CellView({
+				model : cell
+			});
+			cv.render();
+			tr.append(cv.el);
+		}
+		trs.append(tr)
+	}
+	$('#world').html(trs)
+	col = new CellCollection(models);
+
+	setTimeout("step()", stepTime);
 });
