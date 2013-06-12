6a554089982c0d7bf4fda79e50bccc0e012dd610
Tony Boyles <AABoyles@gmail.com>
2013-06-11 23:27:20 -0400

    Cutting down as much of Model as possible.

--- agent.js
+++ agent.js
@@ -1,22 +1,15 @@
-var stepTime = 1;
 var col = undefined;
+var stepTime = 0;
+var numSteps = 0;
 
-function step() {
-	col.each(function(e) {
-		e.computeNextState();
-	});
-	col.each(function(e) {
-		e.transition();
-	});
-	setTimeout("step()", stepTime);
-}
-
-var Agent = Backbone.Model;
-
+var Agent = Backbone.Model.extend({
+	computeNextState : function() {
+	},
+});
 var Population = Backbone.Collection;
-
 var ABM = function(domElement, collections) {
-	run = function() {};
+	run = function() {
+	};
 	geometry = {
 		bounded : {},
 		verticalWrap : {},
@@ -24,38 +17,20 @@ var ABM = function(domElement, collections) {
 		taurus : {},
 		sphere : {}
 	};
-    var $el = domElement || $("body");
-    var colls = collections || [];
+	var $el = domElement || $("body");
+	var colls = collections || [];
 };
 
-//myAgent = new Agent('div', {attributes}, {options});
-//myAgents = new Population(myAgent, {options});
-//myModel = new ABM($('someDOMElement'), [myAgents]);
-//myModel.run();
+function step() {
+	col.each(function(e) {
+		e.next();
+	});
+	col.each(function(e) {
+		e.set('alive', e.get('nextAlive'));
+	});
+	numSteps++;
+	$("#num-steps").text(numSteps);
+}
 
-$(function() {
-	var models = new Array();
-	var trs = $(document.createElement('tbody'));
-	for (var x = 0; x < 100; x++) {
-		var tr = $(document.createElement('tr'));
-		for (var y = 0; y < 100; y++) {
-			var alive = (Math.floor(Math.random() * 100) % 2) == 1;
-			var cell = new Cell({
-				'x' : x,
-				'y' : y,
-				'alive' : alive
-			});
-			models.push(cell);
-			var cv = new CellView({
-				model : cell
-			});
-			cv.render();
-			tr.append(cv.el);
-		}
-		trs.append(tr)
-	}
-	$('#world').html(trs)
-	col = new CellCollection(models);
+var timer = setInterval(step, stepTime);
 
-	setTimeout("step()", stepTime);
-});
